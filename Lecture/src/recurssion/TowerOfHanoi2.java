77416cff8a2e48736e9a62afe2a4f4b66e9ac677
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-07-13 01:37:48 +0530

    java practice

---
+++ Lecture/src/recurssion/TowerOfHanoi2.java
@@ -0,0 +1,24 @@
+package recurssion;
+import java.util.Scanner;
+public class TowerOfHanoi2 {
+    public static void towerOfHanoi(int disks, char a, char b, char c) {
+        // Write your code here
+        if(disks == 1){
+            System.out.println(a + " " + c);
+            return;
+        }
+        towerOfHanoi(disks-1,a,c,b);
+        System.out.println(a + " " + c);
+        towerOfHanoi(disks-1,b,a,c);
+    }
+}
+
+
+
+ class runner22 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        TowerOfHanoi2.towerOfHanoi(n, 'a', 'b', 'c');
+    }
+}
