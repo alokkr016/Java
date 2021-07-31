cd5cbe72fa523c3bc3eaac364b0ac5acec4337c4
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-07-31 12:39:50 +0530

    LinkedList Use with tail too

---
+++ Lecture/src/linkedList/LinkedListUse2.java
@@ -0,0 +1,36 @@
+package linkedList;
+
+import java.util.Scanner;
+
+public class LinkedListUse2 {
+    public static void main(String[] args) {
+        LinkedListNode<Integer> head = takeInput();
+        print(head);
+    }
+
+    private static LinkedListNode<Integer> takeInput() {
+        LinkedListNode<Integer> head = null;
+        LinkedListNode<Integer> tail = null;
+        Scanner s = new Scanner(System.in);
+        int data = s.nextInt();
+        while(data != -1){
+            LinkedListNode<Integer> node = new LinkedListNode<>(data);
+            if(head == null){
+                head = node;
+                tail = node;
+            }
+            else {
+                tail.next = node;
+                tail = tail.next;
+            }
+            data = s.nextInt();
+        }
+        return head;
+    }
+    private static void print(LinkedListNode<Integer> head){
+        while (head != null){
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+}
