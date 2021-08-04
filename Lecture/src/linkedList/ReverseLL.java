e299f5dab32fea83a699567a7509dcd93823700a
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-08-04 13:27:28 +0530

    Reverse a Linked list

---
+++ Lecture/src/linkedList/ReverseLL.java
@@ -0,0 +1,76 @@
+package linkedList;
+/*
+Given a singly linked list of integers, reverse it using recursion and return the head to the modified list.
+You have to do this in O(N) time complexity where N is the size of the linked list.
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class ReverseLL {
+    public static LinkedListNode<Integer> reverseLinkedListRec(LinkedListNode<Integer> head) {
+        //Your code goes here
+        if(head == null || head.next == null){
+            return head;
+        }
+        LinkedListNode<Integer> tail = head.next;
+        LinkedListNode<Integer> finalAns = reverseLinkedListRec(head.next);
+        tail.next = head;
+        head.next = null;
+        return finalAns;
+    }
+
+}
+
+
+ class Runner12 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static LinkedListNode<Integer> takeInput() throws IOException {
+        LinkedListNode<Integer> head = null, tail = null;
+
+        String[] datas = br.readLine().trim().split("\\s");
+
+        int i = 0;
+        while(i < datas.length && !datas[i].equals("-1")) {
+            int data = Integer.parseInt(datas[i]);
+            LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+            if(head == null) {
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            i += 1;
+        }
+
+        return head;
+    }
+
+    public static void print(LinkedListNode<Integer> head) {
+        while(head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t > 0) {
+
+            LinkedListNode<Integer> head = takeInput();
+
+            LinkedListNode<Integer> newHead = ReverseLL.reverseLinkedListRec(head);
+            print(newHead);
+
+            t -= 1;
+        }
+
+    }
+}
