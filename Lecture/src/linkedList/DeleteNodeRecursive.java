af404625879062ccbfd7325bc6260fd6dd8c97ac
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-08-04 22:33:53 +0530

    Deletion of given element at nth Index recursively.

---
+++ Lecture/src/linkedList/DeleteNodeRecursive.java
@@ -0,0 +1,76 @@
+package linkedList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class DeleteNodeRecursive {
+    public static LinkedListNode<Integer> deleteNodeRec(LinkedListNode<Integer> head, int pos) {
+        //Your code goes here
+        if(pos == 0){
+            return head.next;
+        }
+        if(head == null || head.next == null){
+            return head;
+        }
+        head.next = deleteNodeRec(head.next,pos - 1);
+        return head;
+    }
+
+}
+
+
+
+
+ class Runner14 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static LinkedListNode<Integer> takeInput() throws IOException {
+        LinkedListNode<Integer> head = null, tail = null;
+
+        String[] datas = br.readLine().trim().split("\\s");
+
+        int i = 0;
+        while(i < datas.length && !datas[i].equals("-1")) {
+            int data = Integer.parseInt(datas[i]);
+            LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+            if(head == null) {
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            i += 1;
+        }
+
+        return head;
+    }
+
+    public static void print(LinkedListNode<Integer> head) {
+        while(head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t > 0) {
+
+            LinkedListNode<Integer> head = takeInput();
+            int pos = Integer.parseInt(br.readLine().trim());
+
+            LinkedListNode<Integer> newHead = DeleteNodeRecursive.deleteNodeRec(head, pos);
+            print(newHead);
+
+            t -= 1;
+        }
+
+    }
+}
