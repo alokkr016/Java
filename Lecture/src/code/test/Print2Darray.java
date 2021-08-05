9c7f25ccd471860a44e8727db36f7995209a98a5
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-08-05 14:11:48 +0530

    Print 2D array

--- Lecture/src/code/test/Print2Darray.java
+++ Lecture/src/code/test/Print2Darray.java
@@ -1,10 +1,19 @@
 package code.test;
+/*
+Given a 2D integer array with n rows and m columns. Print the 0th row from input n times, 1st row n-1 times…..(n-1)th row will be printed 1 time.
+Input format :
+
+Line 1 : No of rows(n) & No of columns(m) (separated by space)
+
+Line 2 : Row 1 elements (separated by space)
+
+Line 3 : Row 2 elements (separated by space)
+
+Line 4 : and so on
+ */
 import java.util.Scanner;
 public class Print2Darray {
-//    public static void printNo(int i,int input[][]){
-//        int column1 = input[0].length;
-//
-//    }
+
     public static void print2DArray(int input[][]) {
         // Write your code here
         int rows = input.length;
@@ -27,7 +36,26 @@ public class Print2Darray {
 
     }
 }
+/*
+public static void print2DArray(int arr[][]){
+        int rows = arr.length;
+        int columns = arr[0].length;
+        for(int i = 0;i < rows;i++){
+            int j = 0;
 
+            while(j < rows - i){
+                for(int m = 0; m < arr[i].length;m++){
+                    System.out.print(arr[i][m] + " ");
+                }
+                System.out.println();
+                j++;
+            }
+
+        }
+
+}
+}
+ */
 
  class Main4 {
 
