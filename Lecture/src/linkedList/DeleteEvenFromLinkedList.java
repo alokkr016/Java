a1940903847bac0e1e91ee955ce004f9579cbf9c
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-08-06 23:45:28 +0530

    Delete odd elements from the node. Right Answer

--- Lecture/src/linkedList/DeleteEvenFromLinkedList.java
+++ Lecture/src/linkedList/DeleteEvenFromLinkedList.java
@@ -1,25 +1,34 @@
 package linkedList;
 
 public class DeleteEvenFromLinkedList {
-    public static LinkedListNode<Integer> deleteEven(LinkedListNode<Integer> head){
-        if(head == null){
+    public static LinkedListNode<Integer> deleteEven(LinkedListNode<Integer> head) {
+        if (head == null) {
             return head;
         }
-        if(head.next == null){
-            if(head.data % 2 == 0){
+        if (head.next == null) {
+            if (head.data % 2 == 0) {
                 return null;
             }
             return head;
         }
         LinkedListNode<Integer> prev = null;
         LinkedListNode<Integer> curr = head;
-        while(curr != null){
-            if(curr.data % 2 != 0){
+        while (curr != null) {
+            if (curr.data % 2 == 0) {
+                if(prev == null){
+                    head = head.next;
+                    curr = head;
+                }
+                else {
+                    prev.next = curr.next;
+                    curr = curr.next;
+                }
+            }
+            else{
                 prev = curr;
                 curr = curr.next;
             }
-            curr = curr.next;
-            prev.next = curr;
+
         }
         return head;
     }
