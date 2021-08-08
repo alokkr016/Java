c45fa083854b1530f9457edcc8a6f31f3ad11eed
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-08-09 00:11:41 +0530

    Created a dynamic push function

--- Lecture/src/stack/StackUsingArray.java
+++ Lecture/src/stack/StackUsingArray.java
@@ -30,16 +30,24 @@ public class StackUsingArray {
         }
         return data[top];
     }
-    public void push(int element) throws StackFullException{
-        if(size() == data.length){
-            //FullSTackE
-            StackFullException e = new StackFullException();
-            throw e;
-
-        }
-        top++;
-        data[top] = element;
+//    public void push(int element) throws StackFullException{
+//        if(size() == data.length){
+//            //FullSTackE
+//            StackFullException e = new StackFullException();
+//            throw e;
+//
+//        }
+//        top++;
+//        data[top] = element;
+//    }
+public void push(int element) {
+    if(size() == data.length){
+        //FullSTackE
+        doubleCapacity();
     }
+    top++;
+    data[top] = element;
+}
     public int pop() throws StackEmptyException{
         if (size() == 0) {
             StackEmptyException e = new StackEmptyException();
@@ -48,6 +56,13 @@ public class StackUsingArray {
         int temp = data[top];
         top--;
         return temp;
+    }
+    private void doubleCapacity(){
+        int temp[] = data;
+        data = new int[2 * temp.length];
+        for(int i = 0;i < top;i++){
+            data[i] = temp[i];
+        }
     }
 
 }
