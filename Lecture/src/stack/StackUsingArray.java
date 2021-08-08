55de3d77b330a3ea49f7c59fb5e8f6edbfce7502
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-08-08 21:55:46 +0530

    Created a class named StackUsingArray, which defines stack data structure

---
+++ Lecture/src/stack/StackUsingArray.java
@@ -0,0 +1,53 @@
+package stack;
+
+public class StackUsingArray {
+    private int data[];
+    private int top;
+    public StackUsingArray(){
+        data = new int[10];
+        top = -1;
+    }
+    public StackUsingArray(int capacity){
+        data = new int[capacity];
+        top = -1;
+    }
+    public boolean isEmpty(){
+        if(top == -1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public int size(){
+        return top + 1;
+    }
+    public int top() throws StackEmptyException{
+        if(size() == 0){
+            //StackEmptyException
+            StackEmptyException e = new StackEmptyException();
+            throw e;
+        }
+        return data[top];
+    }
+    public void push(int element) throws StackFullException{
+        if(size() == data.length){
+            //FullSTackE
+            StackFullException e = new StackFullException();
+            throw e;
+
+        }
+        top++;
+        data[top] = element;
+    }
+    public int pop() throws StackEmptyException{
+        if (size() == 0) {
+            StackEmptyException e = new StackEmptyException();
+            throw e;
+        }
+        int temp = data[top];
+        top--;
+        return temp;
+    }
+
+}
