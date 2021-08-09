f637c8e791174ff9c730c50a86f75b61640f0cd5
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-08-09 11:31:37 +0530

    Various operation of stack has been used

---
+++ Lecture/src/stack/StackUseLL.java
@@ -0,0 +1,55 @@
+package stack;
+
+import java.util.Scanner;
+//import java.util.Stack;
+
+class Node {
+    int data;
+    NodeStack next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+}
+public class StackUseLL {
+    private static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        StackUsingLL stack = new StackUsingLL();
+
+        int q = s.nextInt();
+
+        while (q > 0) {
+            int choice, input;
+            choice = s.nextInt();
+
+            switch(choice) {
+                case 1:
+                    input = s.nextInt();
+                    stack.push(input);
+                    break;
+
+                case 2:
+                    System.out.println(stack.pop());
+                    break;
+
+                case 3:
+                    System.out.println(stack.top());
+                    break;
+
+                case 4:
+                    System.out.println(stack.getSize());
+                    break;
+
+                default:
+                    System.out.println((stack.isEmpty()) ? "true" : "false");
+            }
+
+            q -= 1;
+        }
+
+    }
+
+}
