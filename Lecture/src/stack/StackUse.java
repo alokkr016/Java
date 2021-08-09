591fef61f63d7c8af2d2ef7c5e6c76dceca7e927
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-08-09 12:03:05 +0530

    Used stack generic class

--- Lecture/src/stack/StackUse.java
+++ Lecture/src/stack/StackUse.java
@@ -2,7 +2,19 @@ package stack;
 
 public class StackUse {
     public static void main(String[] args) throws StackFullException{
-        StackUsingArray stack = new StackUsingArray(3);
+//        StackUsingArray stack = new StackUsingArray(3);
+//        for (int i = 1;i <= 5;i++) {
+//            stack.push(i);
+//        }
+//        while(!stack.isEmpty()){
+//            try {
+//                System.out.println(stack.pop());
+//            }
+//            catch(StackEmptyException e){
+//
+//            }
+//        }
+        StackUsingLLGeneric<Integer> stack = new StackUsingLLGeneric();
         for (int i = 1;i <= 5;i++) {
             stack.push(i);
         }
