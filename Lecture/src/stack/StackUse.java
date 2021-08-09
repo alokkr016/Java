d3dab087b9a9c88275c4febb5f9539a9ae71a645
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-08-09 13:08:20 +0530

    Used stack(LinkedList) generic class

--- Lecture/src/stack/StackUse.java
+++ Lecture/src/stack/StackUse.java
@@ -2,25 +2,27 @@ package stack;
 
 public class StackUse {
     public static void main(String[] args) throws StackFullException{
-//        StackUsingArray stack = new StackUsingArray(3);
-//        for (int i = 1;i <= 5;i++) {
-//            stack.push(i);
-//        }
-//        while(!stack.isEmpty()){
-//            try {
-//                System.out.println(stack.pop());
-//            }
-//            catch(StackEmptyException e){
-//
-//            }
-//        }
-        StackUsingLLGeneric<Integer> stack = new StackUsingLLGeneric();
+        StackUsingArray stack = new StackUsingArray(3);
         for (int i = 1;i <= 5;i++) {
             stack.push(i);
         }
         while(!stack.isEmpty()){
             try {
                 System.out.println(stack.pop());
+            }
+            catch(StackEmptyException e){
+
+            }
+        }
+        //Stack Using LinkedList
+        System.out.println("Stack Using Linked list | ");
+        StackUsingLLGeneric<Integer> stack2 = new StackUsingLLGeneric<>();
+        for (int i = 1;i <= 5;i++) {
+            stack2.push(i);
+        }
+        while(!stack2.isEmpty()){
+            try {
+                System.out.println(stack2.pop());
             }
             catch(StackEmptyException e){
 
