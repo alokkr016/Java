40a6a56afc37016b382dd22af59262b247b70967
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-08-10 15:56:59 +0530

    Created a Queue class Using array

---
+++ Lecture/src/queue/QueueUsingArray.java
@@ -0,0 +1,67 @@
+package queue;
+
+public class QueueUsingArray {
+    private int data[];
+    private int rear;
+    private int front;
+    private int size;
+    public QueueUsingArray(){
+        data =  new int[10];
+        rear = -1;
+        front = -1;
+        size = 0;
+    }
+    public QueueUsingArray(int capacity){
+        data =  new int[capacity];
+        rear = -1;
+        front = -1;
+        size = 0;
+    }
+    int size(){
+        return size;
+    }
+    boolean isEmpty(){
+        return size == 0;
+    }
+    int front() throws QueueEmptyException {
+        if(size == 0){
+            throw new QueueEmptyException();
+        }
+        return data[front];
+    }
+    void enqueue(int element) throws QueueFullException {
+        if(size() == data.length){
+            throw new QueueFullException();
+        }
+        if(size() == 0){
+            front = 0;
+        }
+//        rear++;
+        size++;
+        rear = (rear + 1) % data.length;
+        if(rear == data.length){
+            rear = 0;
+        }
+        data[rear] = element;
+
+    }
+    int dequeue() throws QueueEmptyException {
+        if(size == 0){
+            throw new QueueEmptyException();
+        }
+       int temp = data[front];
+       front++;
+//       front = (front + 1) / data.length;
+       if(front == data.length){
+           front = 0;
+       }
+       size--;
+
+       if (size() == 0){
+           front = -1;
+           rear = -1;
+       }
+       return temp;
+    }
+
+}
