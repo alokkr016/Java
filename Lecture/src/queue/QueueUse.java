e6e8c7450ca42fcc3c1496ae58ade2d722440f4e
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-08-11 20:18:12 +0530

    Modified the class QueueUsingArray to enqueue elements dynamically and used this class in QueueUse class

--- Lecture/src/queue/QueueUse.java
+++ Lecture/src/queue/QueueUse.java
@@ -2,7 +2,7 @@ package queue;
 
 public class QueueUse {
     public static void main(String[] args) {
-        QueueUsingArray queue = new QueueUsingArray();
+        QueueUsingArray queue = new QueueUsingArray(3);
         for (int i = 1; i <= 5; i++) {
             try {
                 queue.enqueue(i);
