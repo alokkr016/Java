a322c41f2b0f9fb9b4421bb5fa719c72042de510
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-08-14 11:56:03 +0530

    Used generic linkedlist program

--- Lecture/src/queue/QueueUse.java
+++ Lecture/src/queue/QueueUse.java
@@ -1,7 +1,7 @@
 package queue;
 
 public class QueueUse {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws QueueEmptyException {
         QueueUsingArray queue = new QueueUsingArray(3);
         for (int i = 1; i <= 5; i++) {
             try {
@@ -16,6 +16,19 @@ public class QueueUse {
                 e.printStackTrace();
             }
         }
-
+        /*
+        =============================================================
+         */
+        QueueUsingLinkedListGeneric<Integer> queue2 = new QueueUsingLinkedListGeneric<>();
+        for (int i = 1; i <= 5; i++) {
+            queue2.enqueue(i);
+        }
+        while (!queue2.isEmpty()) {
+            try {
+                System.out.println(queue2.dequeue());
+            } catch (QueueEmptyException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
