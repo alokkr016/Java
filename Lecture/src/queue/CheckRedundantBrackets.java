b24284c27e789f8a2f546d5fffa890bebb4e81b2
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-08-16 22:24:36 +0530

    Check redundant brackets

--- Lecture/src/queue/CheckRedundantBrackets.java
+++ Lecture/src/queue/CheckRedundantBrackets.java
@@ -3,19 +3,39 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
-
+/*
+For a given expression in the form of a string,
+find if there exist any redundant brackets or not.
+It is given that the expression contains only rounded brackets or parenthesis and
+the input expression will always be balanced.
+A pair of the bracket is said to be redundant when a sub-expression is surrounded by
+unnecessary or needless brackets.
+ */
 public class CheckRedundantBrackets {
     public static boolean checkRedundantBrackets(String expression) {
         //Your code goes here
         Stack<Character> str = new Stack<Character>();
         Boolean bool = false;
-        while(expression != ""){
-            int count = 0;
+        int i = 0;
+        while (i < expression.length()) {
+            if (expression.charAt(i) == ')') {
+                int count = 0;
+                while (str.peek() != '(') {
+                    str.pop();
+                    count++;
+                }
+                str.pop();
+                if (count == 0 || count == 1) {
+                    return true;
+                }
+            } else {
+                str.push(expression.charAt(i));
+            }
+            i++;
         }
+        return bool;
     }
 }
-
-
 
  class Runner12 {
 
