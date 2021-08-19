aa018866c27ca36e046a12a83a471fb58645a690
alokkr016 <alokraj016@outlook.com>
2021-08-19 23:11:16 +0530

    added a takeInput function to take input from the user

--- Lecture/src/tree/TreeUse.java
+++ Lecture/src/tree/TreeUse.java
@@ -3,19 +3,32 @@ package tree;
 import java.util.Scanner;
 
 public class TreeUse {
-
+    public static TreeNode<Integer> takeInput(){
+        int n;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the next node data ");
+        n = s.nextInt();
+        TreeNode<Integer> root = new TreeNode<>(n);
+        System.out.println("Enter the children for " + n);
+        int childCount = s.nextInt();
+        for(int i = 0;i < childCount;i++){
+            TreeNode<Integer> child = takeInput();
+            root.children.add(child);
+        }
+        return root;
+    }
     public static void main(String[] args) {
-//        TreeNode<Integer> root = takeInput();
-        TreeNode<Integer> root = new TreeNode<>(4);
-        TreeNode<Integer> node1 = new TreeNode<>(2);
-        TreeNode<Integer> node2 = new TreeNode<>(8);
-        TreeNode<Integer> node3 = new TreeNode<>(9);
-        TreeNode<Integer> node4 = new TreeNode<>(7);
-        root.children.add(node1);
-        root.children.add(node2);
-        root.children.add(node3);
-        node2.children.add(node4);
-
-        System.out.println(root);
+        TreeNode<Integer> root = takeInput();
+//        TreeNode<Integer> root = new TreeNode<>(4);
+//        TreeNode<Integer> node1 = new TreeNode<>(2);
+//        TreeNode<Integer> node2 = new TreeNode<>(8);
+//        TreeNode<Integer> node3 = new TreeNode<>(9);
+//        TreeNode<Integer> node4 = new TreeNode<>(7);
+//        root.children.add(node1);
+//        root.children.add(node2);
+//        root.children.add(node3);
+//        node2.children.add(node4);
+//
+//        System.out.println(root);
     }
 }
