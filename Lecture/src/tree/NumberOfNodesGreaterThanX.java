9bba92b3a6a33dff0d9401c222aaf37e92776484
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-08-27 23:51:58 +0530

    Created a class NumberOfNodesGreaterThanX to find the number of node in a tree having their value greater than "X"

--- Lecture/src/tree/NumberOfNodesGreaterThanX.java
+++ Lecture/src/tree/NumberOfNodesGreaterThanX.java
@@ -2,14 +2,22 @@ package tree;
 import java.util.Scanner;
 public class NumberOfNodesGreaterThanX {
     public static int numNodeGreater(TreeNode<Integer> root,int x){
+        if(root == null){
+            return Integer.MIN_VALUE;
+        }
+        int count = 0;
+        if(root.data > x){
+            count++;
+        }
+        for(int i = 0; i < root.children.size();i++){
+            count += numNodeGreater(root.children.get(i), x);
 
-        // Write your code here
+        }
+        return count;
 
     }
 
 }
-
-
 
 
 
