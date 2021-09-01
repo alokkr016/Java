8dc07d064ed8faa8dffa1cb4eddfe76cd2303a99
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-09-02 00:40:59 +0530

    Create a takeInput method

--- Lecture/src/binaryTree/BinaryTreeUse.java
+++ Lecture/src/binaryTree/BinaryTreeUse.java
@@ -1,6 +1,22 @@
 package binaryTree;
 
+import java.util.Scanner;
+
 public class BinaryTreeUse {
+
+    public static BinaryTreeNode<Integer> takeInput(Scanner s){
+        int data;
+        System.out.println("Enter the root of the Tree: ");
+        data = s.nextInt();
+        if(data == -1){
+            return null;
+        }
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(data);
+        root.left = takeInput(s);
+        root.right = takeInput(s);
+        return root;
+    }
+
     public static void main(String[] args) {
         BinaryTreeNode<Integer> root = new BinaryTreeNode<>(2);
         BinaryTreeNode<Integer> node1 = new BinaryTreeNode<>(3);
