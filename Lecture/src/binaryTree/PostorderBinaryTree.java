9c1b212d9a3de0cf11b2645e4baf26d1ecf75c6e
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-09-09 23:45:18 +0530

    Created a class PostorderBinaryTree

---
+++ Lecture/src/binaryTree/PostorderBinaryTree.java
@@ -0,0 +1,70 @@
+package binaryTree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PostorderBinaryTree {
+    public static void postOrder(BinaryTreeNode<Integer> root) {
+        //Your code goes here
+        if(root ==null){
+            return;
+        }
+
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+}
+
+class Runner10 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static BinaryTreeNode<Integer> takeInput() throws NumberFormatException, IOException {
+        QueueUsingLL<BinaryTreeNode<Integer>> pendingNodes = new QueueUsingLL<BinaryTreeNode<Integer>>();
+        int start = 0;
+
+        String[] nodeDatas = br.readLine().trim().split(" ");
+
+        int rootData = Integer.parseInt(nodeDatas[start]);
+        start += 1;
+
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+        pendingNodes.enqueue(root);
+
+        while (!pendingNodes.isEmpty()) {
+            BinaryTreeNode<Integer> currentNode;
+            try {
+                currentNode = pendingNodes.dequeue();
+            } catch (QueueEmptyException e) {
+                return null;
+            }
+
+            int leftChildData = Integer.parseInt(nodeDatas[start]);
+            start += 1;
+
+            if (leftChildData != -1) {
+                BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
+                currentNode.left = leftChild;
+                pendingNodes.enqueue(leftChild);
+            }
+
+            int rightChildData = Integer.parseInt(nodeDatas[start]);
+            start += 1;
+
+            if (rightChildData != -1) {
+                BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
+                currentNode.right = rightChild;
+                pendingNodes.enqueue(rightChild);
+            }
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BinaryTreeNode<Integer> root = takeInput();
+        PostorderBinaryTree.postOrder(root);
+    }
+}
