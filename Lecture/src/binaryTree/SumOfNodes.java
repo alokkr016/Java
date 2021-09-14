2269fba7f9baf364ab83cdf89ea1674227197045
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-09-14 23:30:25 +0530

    Sum of the nodes of the given tree

---
+++ Lecture/src/binaryTree/SumOfNodes.java
@@ -0,0 +1,68 @@
+package binaryTree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SumOfNodes {
+    public static int getSum(BinaryTreeNode<Integer> root) {
+        //Your code goes here.
+        if(root == null){
+            return 0;
+        }
+        return root.data + getSum(root.left) + getSum(root.right);
+    }
+
+}
+
+class Runner12 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static BinaryTreeNode<Integer> takeInput() throws NumberFormatException, IOException {
+        QueueUsingLL<BinaryTreeNode<Integer>>  pendingNodes = new QueueUsingLL<BinaryTreeNode<Integer>>();
+        int start = 0;
+
+        String[] nodeDatas = br.readLine().trim().split(" ");
+
+        int rootData = Integer.parseInt(nodeDatas[start]);
+        start += 1;
+
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+        pendingNodes.enqueue(root);
+
+        while(!pendingNodes.isEmpty()){
+            BinaryTreeNode<Integer> currentNode;
+            try {
+                currentNode = pendingNodes.dequeue();
+            } catch (QueueEmptyException e) {
+                return null;
+            }
+
+            int leftChildData = Integer.parseInt(nodeDatas[start]);
+            start += 1;
+
+            if(leftChildData != -1){
+                BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
+                currentNode.left = leftChild;
+                pendingNodes.enqueue(leftChild);
+            }
+
+            int rightChildData = Integer.parseInt(nodeDatas[start]);
+            start += 1;
+
+            if(rightChildData != -1){
+                BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
+                currentNode.right = rightChild;
+                pendingNodes.enqueue(rightChild);
+            }
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BinaryTreeNode<Integer> root = takeInput();
+        System.out.println(SumOfNodes.getSum(root));
+    }
+}
