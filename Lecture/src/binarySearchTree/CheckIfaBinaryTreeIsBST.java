d2835f713a4f9fbbd179608f8be19dfdfff72b01
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-09-27 23:30:33 +0530

    Created a another Improved isBST function

--- Lecture/src/binarySearchTree/CheckIfaBinaryTreeIsBST.java
+++ Lecture/src/binarySearchTree/CheckIfaBinaryTreeIsBST.java
@@ -62,6 +62,20 @@ public class CheckIfaBinaryTreeIsBST {
         return output;
     }
 
+    public static boolean isBST3(BinaryTreeNode<Integer> root, int min, int max){
+        if(root == null){
+            return true;
+        }
+
+        if(root.data < min || root.data > max){
+            return false;
+        }
+
+        boolean isLeftOk = isBST3(root.left, min, root.data -1);
+        boolean isRightOk = isBST3(root.right,root.data + 1, max);
+
+        return isLeftOk && isRightOk;
+    }
 }
 
 
