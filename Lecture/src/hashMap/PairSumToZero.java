07f3e7709d8f306ade9bd81ff4ac2f22ecd05580
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-10-26 20:39:08 +0530

    Pair sum zero

--- Lecture/src/hashMap/PairSumToZero.java
+++ Lecture/src/hashMap/PairSumToZero.java
@@ -10,73 +10,72 @@ Find and print the count of pair of elements in the array which sum up to 0.
 */
 public class PairSumToZero {
     public static int PairSum(int[] input, int size) {
-    HashMap<Integer,Integer> hm = new HashMap<>();
-    for(int i = 0;i < input.length;i++){
-        if(!hm.containsKey(input[i])){
-            hm.put(input[i],1);
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i = 0;i < input.length;i++){
+            if(!hm.containsKey(input[i])){
+                hm.put(input[i],1);
+            }
+            else{
+                hm.put(input[i],hm.get(input[i]) + 1);
+            }
         }
-        else{
-            hm.put(input[i],hm.get(input[i]) + 1);
-        }
-    }
-    int count = 0;
-    for(int i = 0;i < input.length;i++){
-        if(hm.containsKey(-input[i])){
-            int times;
-            if(input[i] == -input[i]){
-                int occ = hm.get(input[i]);
-                times = (occ * (occ - 1)) / 2;
+        int count = 0;
+        for(int i = 0;i < input.length;i++){
+            if(hm.containsKey(-input[i])){
+                int times;
+                if(input[i] == -input[i]){
+                    int occ = hm.get(input[i]);
+                    times = (occ * (occ - 1)) / 2;
+                    count += times;
+                    hm.put(input[i],0);
+                    continue;
+                }
+                times = hm.get(input[i]) * hm.get(-input[i]);
                 count += times;
                 hm.put(input[i],0);
-                continue;
+                hm.put(-input[i],0);
             }
-            times = hm.get(input[i]) * hm.get(-input[i]);
-            count += times;
-            hm.put(input[i],0);
-            hm.put(-input[i],0);
         }
+        return count;
     }
-    return count;
 }
 
+    class Runner3 {
 
+        static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-class Runner3 {
+        public static int[] takeInput() throws IOException {
+            int size = Integer.parseInt(br.readLine().trim());
+            int[] input = new int[size];
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            if (size == 0) {
+                return input;
+            }
 
-    public static int[] takeInput() throws IOException {
-        int size = Integer.parseInt(br.readLine().trim());
-        int[] input = new int[size];
+            String[] strNums;
+            strNums = br.readLine().split("\\s");
 
-        if (size == 0) {
+            for (int i = 0; i < size; ++i) {
+                input[i] = Integer.parseInt(strNums[i]);
+            }
+
             return input;
         }
 
-        String[] strNums;
-        strNums = br.readLine().split("\\s");
+        public static void printArray(int[] arr) {
+            for (int element : arr) {
+                System.out.print(element + " ");
+            }
 
-        for (int i = 0; i < size; ++i) {
-            input[i] = Integer.parseInt(strNums[i]);
+            System.out.println();
         }
 
-        return input;
-    }
+        public static void main(String[] args) throws NumberFormatException, IOException {
 
-    public static void printArray(int[] arr) {
-        for (int element : arr) {
-            System.out.print(element + " ");
+
+            int[] arr = takeInput();
+            System.out.print(PairSumToZero.PairSum(arr, arr.length));
+
+
         }
-
-        System.out.println();
     }
-
-    public static void main(String[] args) throws NumberFormatException, IOException {
-
-
-        int[] arr = takeInput();
-        System.out.print(PairSumToZero.PairSum(arr,arr.length));
-
-
-    }
-}
