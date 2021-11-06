6a686cea24c6eec66d2e19cd37d21174143a29fc
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-11-07 02:21:46 +0530

    Inbuilt MaxPriorityQueue and MinPriorityQueue implementations.

--- Lecture/src/priorityQueues/PriorityQueueUse.java
+++ Lecture/src/priorityQueues/PriorityQueueUse.java
@@ -1,15 +1,53 @@
 package priorityQueues;
 
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class MinPriorityQueueComparator implements Comparator<Integer>{
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        if(o1 < o2){
+            return -1;
+        }
+        else if(o1 > o2){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
+class MaxPriorityQueueComparator implements Comparator<Integer>{
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        if(o1 < o2){
+            return 1;
+        }
+        else if(o1 > o2){
+            return -1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
+
 public class PriorityQueueUse {
     public static void main(String[] args) throws PriorityQueueException {
-        MinPriorityQueues pq = new MinPriorityQueues();
+//        MinPriorityQueues pq = new MinPriorityQueues();
+
+        MaxPriorityQueueComparator maxComparator = new MaxPriorityQueueComparator();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(maxComparator);
         int arr[] = {5, 1, 9, 2, 0};
         for (int i = 0; i < arr.length; i++) {
-            pq.insert(arr[i]);
+            pq.add(arr[i]);
         }
 
         while (!pq.isEmpty()) {
-            System.out.println(pq.delete() + " ");
+            System.out.println(pq.remove() + " ");
         }
         System.out.println();
     }
