465f226418a0c4923647e781de1fcff9f5a670d8
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-11-07 02:28:49 +0530

    Implemented Priority Queue for a String

--- Lecture/src/priorityQueues/PriorityQueueUse.java
+++ Lecture/src/priorityQueues/PriorityQueueUse.java
@@ -35,13 +35,28 @@ class MaxPriorityQueueComparator implements Comparator<Integer>{
     }
 }
 
+class StringLengthCompapator implements Comparator<String>{
+
+    @Override
+    public int compare(String o1, String o2) {
+        if(o1.length() < o2.length()){
+            return -1;
+        }else if(o1.length() > o2.length()){
+            return 1;
+        } else{
+            return 0;
+        }
+    }
+}
 public class PriorityQueueUse {
     public static void main(String[] args) throws PriorityQueueException {
+        StringLengthCompapator stringComparator = new StringLengthCompapator();
+        PriorityQueue<String> pq = new PriorityQueue<String>(stringComparator);
 //        MinPriorityQueues pq = new MinPriorityQueues();
-
-        MaxPriorityQueueComparator maxComparator = new MaxPriorityQueueComparator();
-        PriorityQueue<Integer> pq = new PriorityQueue<>(maxComparator);
-        int arr[] = {5, 1, 9, 2, 0};
+//        MaxPriorityQueueComparator maxComparator = new MaxPriorityQueueComparator();
+//        PriorityQueue<Integer> pq = new PriorityQueue<>(maxComparator);
+//        int arr[] = {5, 1, 9, 2, 0};
+        String arr[] = {"I", "Boy", "Good", "AM"};
         for (int i = 0; i < arr.length; i++) {
             pq.add(arr[i]);
         }
